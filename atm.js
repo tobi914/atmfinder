ceee33cdc16b395b8b5ca3d3fa92708918d1a5fb
Tobias Raggl <tobias.raggl@yandex.com>
2018-05-25 12:08:07 +0200

    added hidden minimap to each row, shown by clicking on it

--- atm.js
+++ atm.js
@@ -3,6 +3,7 @@ var infowindow;
 var service;
 
 function initialize(){
+  document.getElementById("hidden").style.display = "none";
   map = new google.maps.Map(document.getElementById("map"), {
     zoom:13
   });
@@ -18,6 +19,12 @@ function updateLocation(){
   locationPromise.then((pos) => {
     var loc = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
     map.setCenter(loc);
+    var marker = new google.maps.Marker({
+      map:map,
+      icon:"http://maps.google.com/mapfiles/ms/icons/blue-dot.png",
+      label:"Your Position",
+      position:loc
+    });
     updateMarker(1000, ["atm"], loc);
   })
   locationPromise.catch((pos_err) => {
@@ -28,14 +35,26 @@ function updateLocation(){
 function callback(results, status, loc){
   if(status == google.maps.places.PlacesServiceStatus.OK){
     var tbodyId = document.getElementById("atmList");
+    var link;
     results = distanceSort(results, loc);
     for(var i = 0; i < 10; ++i){
       mark(results[i]);
+      link = "https://maps.googleapis.com/maps/api/staticmap?center=" + loc.lat() + "," + loc.lng() + "&size=400x300&markers=color:blue|size:tiny|" + loc.lat() + "," + loc.lng() + "&markers=color:red|size:mid|" + results[i].geometry.location.lat() + "," + results[i].geometry.location.lng() + "&key=AIzaSyBP-BLiqXxf5pnZdnZYz9diITQfKkvoqWY";
+      console.log(link);
       var row = tbodyId.insertRow(-1);
       var namecell = row.insertCell(0);
       var distcell = row.insertCell(1);
+      var hcell = row.insertCell(2);
+      hcell.style.display = "none";
+      (function(row, hcell){
+        row.addEventListener("click", () => {
+          toggle(hcell);
+        });
+      }(row, hcell));
       namecell.innerHTML = results[i].name;
       distcell.innerHTML = results[i].distance.toFixed(0) + "m";
+      hcell.innerHTML = document.createElement("IMG");
+      hcell.innerHTML = "<img src=" + link + ">";
     }
   }
 }
@@ -43,6 +62,7 @@ function callback(results, status, loc){
 function mark(place){
   var marker = new google.maps.Marker({
     map:map,
+    animation: google.maps.Animation.DROP,
     position:place.geometry.location
   });
   google.maps.event.addListener(marker, "click", () => {
@@ -54,8 +74,9 @@ function mark(place){
 function updateMarker(rad, type, loc){
   var request = {
     location:loc,
-    radius:rad,
-    types:type
+    //radius:rad,
+    types:type,
+    rankBy:google.maps.places.RankBy.DISTANCE
   };
   service.nearbySearch(request, (result, status) => {
     callback(result, status, loc);
@@ -111,6 +132,17 @@ function sortTable(index)
       rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
       switching = true;
     }
+  }
+}
+
+function toggle(cell){
+  if(cell.style.display === "none"){
+    cell.style.display = "block";
+    document.getElementById("hidden").style.display = "block";
+  }
+  else{
+    cell.style.display = "none";
+    document.getElementById("hidden").style.display = "none";
   }
 }
 
